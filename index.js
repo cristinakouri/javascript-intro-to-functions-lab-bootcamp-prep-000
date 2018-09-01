d8def8f68e17e31ff633c73fc0eccbdd7e35235a
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-09-01 17:34:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ whisper("hi")
 function logShout(string) {
   console.log(string.toUpperCase());
 }
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return `I can't hear you!`
+  }
+}
